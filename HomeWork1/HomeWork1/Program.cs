5b7fbdbad0ce00c4b0e427ad1910ad6dc81fe5d3
Ildar Sadykov <il27@bk.ru>
2020-12-07 23:03:42 +0500

    fisrt task completed

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стоимость звонка: ");
+            int InputCost = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите номер дня недели(0-Воскресенье, 1-Понедельник ...)");
+            int InputDay = Convert.ToInt32(Console.ReadLine());
+            if(InputDay == 0 || InputDay == 6)
+            {
+                double FinalCost = InputCost * 0.8;
+                Console.WriteLine("Стоимость вашего звонка составила " + FinalCost + " у.е.");
+
+            }
+            else
+            {
+                Console.WriteLine("Стоимость вашего звонка не изменилась");
+            }
+        }
+    }
+}
